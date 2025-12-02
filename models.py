07e259a06f83d0059f6ff559297669fac1df966e
thanh-phuc-admin <phucnchau@gmail.com>
2025-12-01 21:10:58 -0500

    Initial commit

---
+++ models.py
@@ -0,0 +1,25 @@
+# Define multiple models for various datasets
+
+from tensorflow.keras import layers, models
+
+
+def create_cnn_1(input_shape):
+    model = models.Sequential()
+
+
+
+    return model
+
+def create_cnn_2(input_shape):
+    model = models.Sequential()
+
+
+
+    return model
+
+def create_cnn_3(input_shape):
+    model = models.Sequential()
+
+
+
+    return model
